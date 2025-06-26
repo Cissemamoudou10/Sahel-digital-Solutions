56cc76a1c60a785087e525d5a123cd5b413a95ec
Cissemamoudou10 <Cisseboston@gmail.com>
2025-06-26 19:24:45 +0000

    preparation du backend avant deploiement

--- api/app.js
+++ api/app.js
@@ -3,12 +3,21 @@ const cors = require("cors");
 const produitRoutes = require("./routes/produitRoutes");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
-const realisationRoutes = require("./routes/realisationRoutes")
+const realisationRoutes = require("./routes/realisationRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, // ex: "https://monfrontend.com"
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
